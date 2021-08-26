000c2c0419cb10d66c1e0befff2ebe16ecee4b62
sashakovtun13 <47829584+sashakovtun13@users.noreply.github.com>
2021-08-26 18:14:33 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,24 +1,37 @@
 let data_book;
-let next_page = 0;
-let output = "";
+let next_page;
+let output;
 let test = $(".page");
+let save = 1;
+
+
+localStorage.setItem('myCat', 'Tom');
+
+let cat = localStorage.getItem('myCat');
+console.log(cat);
 
 f_load();
-
 get_inp();
 next_page_fun();
+save_page();
+
+
+
+// function
 
 function f_load() {
     $.getJSON("./data/goosebumps.json",
         function(data) {
-            console.log(data);
+
             output = "";
-            $.each(data, function(key, val) {
+            data_book = data;
+
+            console.log(data_book);
+            $.each(data_book, function(key, val) {
 
                 if (key == 1) {
                     output += "<p id='page'>" + val.page + "</p>";
                     output += "<img src=" + val.img + ">";
-
                     test.html(output);
 
                 }
@@ -29,25 +42,20 @@ function f_load() {
 }
 
 function load_page(page) {
-    $.getJSON("./data/goosebumps.json",
-        function(data) {
-            console.log(data);
-            output = "";
-            $.each(data, function(key, val) {
+    output = "";
+    $.each(data_book, function(key, val) {
 
-                if (key == page) {
-                    output += "<p id='page'>" + val.page + "</p>";
-                    output += "<p class='text'>" + val.text + "</p>";
-                    output += "<p class='g_to'>" + val.g_to + "</p>";
-                    test.html(output);
-                }
-            });
-
+        if (key == page) {
+            output += "<p  id='page'>" + val.page + "</p>";
+            output += "<p class=' text'>" + val.text + "</p>";
+            output += "<p class=' g_to'>" + val.g_to + "</p>";
+            test.html(output);
         }
-    );
+    });
+
+
+
 }
-
-
 
 function get_inp() {
     $('#get_inp').click(function() {
@@ -69,3 +77,23 @@ function next_page_fun() {
 
     });
 }
+
+function json_st() {
+    aa = $(".aa");
+    let a;
+
+    for (let index = 11; index < 136; index++) {
+
+        a += '\"' + index + '\": { <br> \"page\":' + index + ', <br> \"text\":\"\", <br> \"g_to\": \"\" <br> },';
+        aa.html(a);
+
+    }
+}
+
+// function save_page() {
+//     $('#save').click(function() {
+//         next_page = (parseInt($('#page').text())) + 1;
+//         load_page(next_page);
+
+//     });
+// }
